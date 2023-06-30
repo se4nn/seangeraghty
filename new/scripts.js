fd7df58c68e27adc5f901b551921a8f79b342519
se4nn <sean@geraghty.co>
2023-06-30 13:21:37 +0100

    Initial commit

---
+++ new/scripts.js
@@ -0,0 +1,42 @@
+function transformScroll(event) {
+  if (!event.deltaY) {
+    return;
+  }
+
+  event.currentTarget.scrollLeft += event.deltaY + event.deltaX;
+  event.preventDefault();
+}
+
+var element = document.scrollingElement || document.documentElement;
+element.addEventListener('wheel', transformScroll);
+
+
+$(window).scroll(function() {
+  
+  // selectors
+  var $window = $(window),
+      $body = $('body'),
+      $panel = $('.panel');
+  
+  // Change 33% earlier than scroll position so colour is there when you arrive.
+  var scroll = $window.scrollLeft() + ($window.height() / 3);
+ 
+  $panel.each(function () {
+    var $this = $(this);
+    
+    // if position is within range of this panel.
+    // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
+    // Remember we set the scroll to 33% earlier in scroll var.
+    if ($this.position().left <= scroll && $this.position().left + $this.height() > scroll) {
+          
+      // Remove all classes on body with color-
+      $body.removeClass(function (index, css) {
+        return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
+      });
+       
+      // Add class of currently active div
+      $body.addClass('color-' + $(this).data('color'));
+    }
+  });    
+  
+}).scroll();
