c3c6246b05eac461dccb67807fc08227575c1bdd
se4nn <sean@geraghty.co>
2023-07-02 21:06:23 +0100

    Some updates
    Keep failing to initialise the JS

--- new/scripts.js
+++ new/scripts.js
@@ -6,7 +6,7 @@ function calcDynamicHeight(ref) {
   const vw = window.innerWidth;
   const vh = window.innerHeight;
   const objectWidth = ref.scrollWidth;
-  return objectWidth - vw + vh - 96;
+  return objectWidth - vw + vh - (vw * 0.04);
 }
 
 window.addEventListener('scroll', () => {
@@ -18,32 +18,32 @@ window.addEventListener('resize', () => {
   spaceHolder.style.height = `${calcDynamicHeight(horizontal)}px`;
 });
 
-// $(window).scroll(function() {
+$(window).scroll(function() {
   
-//   // selectors
-//   var $window = $(window),
-//       $body = $('body'),
-//       $panel = $('.panel');
+  // selectors
+  var $window = $(window),
+      $body = $('body'),
+      $panel = $('.panel');
   
-//   // Change 33% earlier than scroll position so colour is there when you arrive.
-//   var scroll = $window.scrollLeft() + ($window.width() / 3);
+  // Change 33% earlier than scroll position so colour is there when you arrive.
+  var scroll = $window.scrollTop() + ($window.height() / 3);
  
-//   $panel.each(function () {
-//     var $this = $(this);
+  $panel.each(function () {
+    var $this = $(this);
     
-//     // if position is within range of this panel.
-//     // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
-//     // Remember we set the scroll to 33% earlier in scroll var.
-//     if ($this.position().left <= scroll && $this.position().left + $this.width() > scroll) {
+    // if position is within range of this panel.
+    // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
+    // Remember we set the scroll to 33% earlier in scroll var.
+    if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
           
-//       // Remove all classes on body with color-
-//       $body.removeClass(function (index, css) {
-//         return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
-//       });
+      // Remove all classes on body with color-
+      $body.removeClass(function (index, css) {
+        return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
+      });
        
-//       // Add class of currently active div
-//       $body.addClass('color-' + $(this).data('color'));
-//     }
-//   });    
+      // Add class of currently active div
+      $body.addClass('color-' + $(this).data('color'));
+    }
+  });    
   
-// }).scroll();
+}).scroll();
