06497c1e2a674135407f39db0cb3ea034cb7a974
se4nn <sean@geraghty.co>
2023-07-02 17:37:37 +0100

    Updated with scrolling fixed
    Broke the colour change though...

--- new/scripts.js
+++ new/scripts.js
@@ -1,34 +1,49 @@
-$(window).scroll(function() {
-  
-  var $window = $(window),
-      $body = $('body'),
-      $panel = $('.panel');
-  
-  var scroll = $window.scrollLeft() + ($window.height() / 3);
- 
-  $panel.each(function () {
-    var $this = $(this);
-    
-    if ($this.position().left <= scroll && $this.position().left + $this.height() > scroll) {
-          
-      $body.removeClass(function (index, css) {
-        return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
-      });
-       
-      $body.addClass('color-' + $(this).data('color'));
-    }
-  });    
-  
-}).scroll();
+const spaceHolder = document.querySelector('.space-holder');
+const horizontal = document.querySelector('.horizontal');
+spaceHolder.style.height = `${calcDynamicHeight(horizontal)}px`;
 
-function transformScroll(event) {
-  if (!event.deltaY) {
-    return;
-  }
-
-  event.currentTarget.scrollLeft += event.deltaY + event.deltaX;
-  event.preventDefault();
+function calcDynamicHeight(ref) {
+  const vw = window.innerWidth;
+  const vh = window.innerHeight;
+  const objectWidth = ref.scrollWidth;
+  return objectWidth - vw + vh;
 }
 
-var element = document.scrollingElement || document.documentElement;
-element.addEventListener('wheel', transformScroll);
+window.addEventListener('scroll', () => {
+  const sticky = document.querySelector('.sticky');
+  horizontal.style.transform = `translateX(-${sticky.offsetTop}px)`;
+});
+
+window.addEventListener('resize', () => {
+  spaceHolder.style.height = `${calcDynamicHeight(horizontal)}px`;
+});
+
+// $(window).scroll(function() {
+  
+//   // selectors
+//   var $window = $(window),
+//       $body = $('body'),
+//       $panel = $('.panel');
+  
+//   // Change 33% earlier than scroll position so colour is there when you arrive.
+//   var scroll = $window.scrollLeft() + ($window.width() / 3);
+ 
+//   $panel.each(function () {
+//     var $this = $(this);
+    
+//     // if position is within range of this panel.
+//     // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
+//     // Remember we set the scroll to 33% earlier in scroll var.
+//     if ($this.position().left <= scroll && $this.position().left + $this.width() > scroll) {
+          
+//       // Remove all classes on body with color-
+//       $body.removeClass(function (index, css) {
+//         return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
+//       });
+       
+//       // Add class of currently active div
+//       $body.addClass('color-' + $(this).data('color'));
+//     }
+//   });    
+  
+// }).scroll();
